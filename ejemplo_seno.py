33df1d522923925bbdd1afe8a606b9175feedbaa
TatiFlores <tatiflores.4@gmail.com>
2016-09-11 22:40:37 -0300

    Añade el codigo a utilizar

---
+++ ejemplo_seno.py
@@ -0,0 +1,11 @@
+'''
+En este codigo se plotea la funcion Seno
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0,15)
+plt.plot(x, np.sin(x), 'o')
+plt.savefig("Seno")
+plt.show()
